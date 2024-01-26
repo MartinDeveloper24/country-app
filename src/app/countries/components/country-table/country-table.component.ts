3a02d1ec987f29a6b1103e2abc96f03ca2325ad2
Developer24 <developer24@grupotransforma.mx>
2024-01-26 15:27:16 -0600

    124 - CountryTable Component Finished

--- src/app/countries/components/country-table/country-table.component.ts
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/country';
 
 @Component({
-  selector: 'app-country-table',
+  selector: 'countries-table',
   templateUrl: './country-table.component.html',
   styles: [
+    `
+    img {
+      width: 25px;
+    }
+    `
   ]
 })
 export class CountryTableComponent {
-
+  @Input() public countries: Country[] = [];
 }
